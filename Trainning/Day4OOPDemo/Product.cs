775f6b21c11371ccc3d66a21299d22d9e8de5980
Ayush Kumar Mishra <ayushkumar.12212147@lpu.in>
2025-12-23 09:33:43 +0530

    The Practice code of Day 4

---
+++ Trainning/Day4OOPDemo/Product.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Day4OOPDemo
+{
+    /// <summary>
+    /// Product entity class as it has product data variables
+    /// For Demo Purpose Only
+    /// </summary>
+    public class Product
+    {
+
+        public int ProdId{get;set;}
+        public string Name{get;set;}
+        public int Price{set;get;}
+        
+    }
+}
