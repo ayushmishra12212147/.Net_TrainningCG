16e6af58e5ad1fb11d6359f6a055d3e321f7060a
Ayush Kumar Mishra <ayushkumar.12212147@lpu.in>
2026-01-13 10:06:10 +0530

    Add files via upload

---
+++ Trainning/Day4OOPDEMO/InterfaceDemo.cs
@@ -0,0 +1,46 @@
+using System;
+namespace Day4OOPDemo
+{
+    interface IAdd
+    {
+        int AddMe(int num1,int num2);
+    }
+
+
+    interface ISub
+    {
+        int SubMe(int num1,int num2);
+    }
+
+
+
+    interface IProd
+    {
+        int ProdMe(int num1,int num2);
+    }
+
+
+    interface IDiv
+    {
+        float DivMe(int num1,int num2);
+    }
+
+    interface IAddSub : IAdd, ISub
+    {
+        
+    }
+    interface IaddProdDiv : IAdd, IProd, IDiv
+    {
+        
+    }
+
+    interface IAll: IaddProdDiv, ISub
+    {
+        
+    }
+
+
+
+
+
+}
---
+++ Trainning/Day4OOPDEMO/Program.cs
@@ -0,0 +1,38 @@
+﻿using Day4OOPDemo;
+
+
+
+System.Console.WriteLine("Code Running");
+
+
+// IAdd m1=new MathClass();
+// IAddSub m2=new MathClass();
+// IAll m3=new MathClass();
+
+// m1.add(2,3);
+
+
+
+
+//Old Way to give Value 
+Product pro=new Product();
+pro.Name="Abc";
+pro.ProdId=123;
+
+//Object Initilizer
+Product obj=new Product(){ProdId=123,Name="Brorsil",Price=2222};
+
+//Collection Initilizer
+List<Product> prodList=new List<Product>()
+{
+    new Product(){ProdId=101,Name="Brorsil",Price=225},
+    new Product(){ProdId=102,Name="Vicks",Price=20},
+    new Product(){ProdId=103,Name="Body Lotion",Price=286},
+    new Product(){ProdId=104,Name="Marker",Price=25},
+};
+
+
+foreach(var item in prodList)
+{
+    System.Console.WriteLine($"{item.ProdId}\t{item.Name}\t{item.Price}");
+}
