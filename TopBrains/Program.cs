8020e6f602028e2c48a178704e3a048832556bb8
Ayush Kumar Mishra <ayushkumar.12212147@lpu.in>
2026-01-31 12:39:45 +0530

    Add files via upload

---
+++ TopBrains/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TopBrains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Q22Swapping.Q22Main();
+            Q23Multiplication.Q23Main();
+            Console.WriteLine(DisplayHeight.GetHeightCategory(120));
+            
+            
+        }
+    }
+}
